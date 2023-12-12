d93b837104ff6dba9d541efae7a0c446009f46f9
cosmoscoder237 <86369403+cosmoscoder237@users.noreply.github.com>
2023-12-12 17:34:59 +0700

    Update bossRush.js

--- server/modules/gamemodes/bossRush.js
+++ server/modules/gamemodes/bossRush.js
@@ -40,6 +40,7 @@ class BossRush {
             [ 15, "ezekiel"],
             [ 15, "eris"],
             [ 15, "selene"],
+            [ 15, "shonen"],
 
             //celestials
             [ 35, "paladin"],
